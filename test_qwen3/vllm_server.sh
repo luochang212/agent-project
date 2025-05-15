0fdd9a7679e99578450a58f4ae7e07d58503bb24
Chang Luo <33987852+luochang212@users.noreply.github.com>
2025-05-16 01:47:13 +0800

    update

--- test_qwen3/vllm_server.sh
+++ test_qwen3/vllm_server.sh
@@ -4,15 +4,18 @@
 # HELP: vllm serve --help
 # REFS:
 #   - https://github.com/luochang212/llm-deploy/blob/main/server/qwen_vllm_bash_server.sh
+#   - https://github.com/QwenLM/Qwen-Agent/tree/main?tab=readme-ov-file#preparation-model-service
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate qwen3_vllm
 
-qwen3_model_path="../model/Qwen/Qwen3-0.6B-FP8"
-vllm serve $qwen3_model_path \
-    --served-model-name Qwen3-0.6B-FP8 \
+model_name="Qwen3-0.6B-FP8"
+CUDA_VISIBLE_DEVICES=0 vllm serve "../model/Qwen/${model_name}" \
+    --served-model-name ${model_name} \
     --host 0.0.0.0 \
     --port 8000 \
     --gpu-memory-utilization 0.98 \
     --tensor-parallel-size 1 \
+    --enable-reasoning \
+    --reasoning-parser deepseek_r1 \
     --api-key token-kcgyrk
